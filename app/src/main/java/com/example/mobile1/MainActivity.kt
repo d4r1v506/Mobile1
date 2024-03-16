e38ac32c4deb5042236bd489777533cb50430579
dario <djfalconi@hotmail.com>
2024-03-16 14:22:20 -0500

    Modificadores de color, posicion y alineacion

--- app/src/main/java/com/example/mobile1/MainActivity.kt
+++ app/src/main/java/com/example/mobile1/MainActivity.kt
@@ -6,15 +6,12 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.size
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
-import androidx.compose.ui.Alignment.Companion.Start
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
@@ -56,7 +53,7 @@ fun Greeting(name: String) {
             fontSize = 40.sp,
             modifier = Modifier
                 .background(Color.Red)
-                // .padding(16.sp)
+                //.padding(16.sp)
                 .background(Color.Blue)
         )
         Text(
